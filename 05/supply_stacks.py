20086e3d6f03ff854e73bb621d79a21916e52430
jangboolee <github@jangboolee.com>
2023-01-11 11:48:58 +0900

    Finish supply stacks and create unit test

--- 05/supply_stacks.py
+++ 05/supply_stacks.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 
 # Set directories for folder and input file
@@ -31,7 +32,7 @@ def read_input(file_name):
     # Separate the raw stacks, instructions, and find the stack count
     stacks_raw = f_content[0:separator - 1]
     instructions = f_content[separator + 1:]
-    stack_count = max([int(num) for num in f_content[separator-1].split()])
+    stack_count = max([int(num) for num in f_content[separator - 1].split()])
 
     return stacks_raw, instructions, stack_count
 
@@ -40,34 +41,105 @@ def parse_stacks(stacks_raw, stack_count):
     """Parse in list of raw stacks as a dictionary
 
     Args:
-        stacks_raw (list): List of each line as an item
+        stacks_raw (list): List of each stack of crates
         stack_count (int): Count of stacks identified in the input file
 
     Returns:
-        dict: Dictionary representation of the stacks
+        dict: Dictionary representation of the stacks as a list for each stack
     """
 
     # Extract the stack letters only from each line
     parsed = [line[1::4] for line in stacks_raw]
 
-    # Create an empty dictionary for each stack
-    stacks_dict = {k: [] for k in range(1, stack_count+1)}
+    # Initialize an empty dictionary for each stack
+    stacks_dict = {k: [] for k in range(1, stack_count + 1)}
 
-    # Generate a dictionary of lists representing the stack
+    # Update the dictionary of lists representing the stack
     for col in range(1, stack_count + 1):
         for row in range(len(parsed)):
             try:
-                stacks_dict[col].append(parsed[row][col - 1])
+                if parsed[row][col - 1] != ' ':
+                    stacks_dict[col].append(parsed[row][col - 1])
             except IndexError:
-                stacks_dict[col].append(' ')
+                pass
 
     return stacks_dict
 
 
+def operate_crane_mover_9000(parsed_stacks, instruction):
+    """Move crate one by one, reversing order when moving, according to single
+    operation instruction
+
+    Args:
+        parsed_stacks (dict): Dictionary representation of crate stacks
+        instruction (str): Single crate operation instruction
+    """
+
+    # Parse key elements from the crane operation instructions
+    pattern = re.compile(r"move (\d+) from (\d+) to (\d+)", re.IGNORECASE)
+    matches = re.match(pattern, instruction)
+
+    # Extract key elements from parsed result
+    if matches:
+        count, origin, destination = [int(match) for match in matches.groups()]
+
+    # Move specified count of crates
+    while count > 0:
+
+        # Identify the crate to be moved
+        move_crate = parsed_stacks[origin][0]
+
+        # Add the crate to the destination and remove it from the origin
+        parsed_stacks[destination].insert(0, move_crate)
+        parsed_stacks[origin].pop(0)
+
+        count -= 1
+
+
+def operate_crane_mover_9001(parsed_stacks, instruction):
+    """Move crates at once, retaining order when moving, according to single
+    operation instruction
+
+    Args:
+        parsed_stacks (dict): Dictionary representation of crate stacks
+        instruction (str): Single crate operation instruction
+    """
+
+    # Parse key elements from the crane operation instructions
+    pattern = re.compile(r"move (\d+) from (\d+) to (\d+)", re.IGNORECASE)
+    matches = re.match(pattern, instruction)
+
+    # Extract key elements from parsed result
+    if matches:
+        count, origin, destination = [int(match) for match in matches.groups()]
+
+    # Identify the crates to be moved
+    move_crates = parsed_stacks[origin][0:count]
+
+    # Add the crates to the destination and remove them from the origin
+    for move_crate in move_crates[::-1]:
+        parsed_stacks[destination].insert(0, move_crate)
+        parsed_stacks[origin].pop(0)
+
+
 def main():
 
-    stacks, instructions, stack_count = read_input(input_file)
-    parsed_stacks = parse_stacks(stacks, stack_count)
+    raw_stacks, instructions, stack_count = read_input(input_file)
+    parsed_stacks = parse_stacks(raw_stacks, stack_count)
+    for instruction in instructions:
+        operate_crane_mover_9000(parsed_stacks, instruction)
+
+    # Solution for part 1
+    print('The top crates after crane mover 9000 are: ')
+    print(''.join([stack[0] for stack in parsed_stacks.values()]))
+
+    parsed_stacks = parse_stacks(raw_stacks, stack_count)
+    for instruction in instructions:
+        operate_crane_mover_9001(parsed_stacks, instruction)
+
+    # Solution for part 2
+    print('The top crates after crane mover 9001 are: ')
+    print(''.join([stack[0] for stack in parsed_stacks.values()]))
 
 
 if __name__ == '__main__':
--- 05/test_supply_stacks.py
+++ 05/test_supply_stacks.py
@@ -6,17 +6,94 @@ class TestSupplyStacks(unittest.TestCase):
 
     def test_parse_stacks(self):
 
+        # Test example stack
         raw_1 = ['    [D]    ',
                  '[N] [C]    ',
                  '[Z] [M] [P]']
 
         actual_1 = supply_stacks.parse_stacks(raw_1, 3)
 
-        expected_1 = {1: [' ', 'N', 'Z'],
+        expected_1 = {1: ['N', 'Z'],
                       2: ['D', 'C', 'M'],
-                      3: [' ', ' ', 'P']}
+                      3: ['P']}
 
         self.assertDictEqual(actual_1, expected_1)
+
+        # Test singular stack
+        raw_2 = ['[A]',
+                 '[B]',
+                 '[C]',
+                 '[D]']
+
+        actual_2 = supply_stacks.parse_stacks(raw_2, 1)
+
+        expected_2 = {1: ['A', 'B', 'C', 'D']}
+
+        self.assertDictEqual(actual_2, expected_2)
+
+    def test_operate_crate_mover_9000(self):
+
+        # Test first instruction on example stack
+        raw_1 = ['    [D]    ',
+                 '[N] [C]    ',
+                 '[Z] [M] [P]']
+
+        actual_1 = supply_stacks.parse_stacks(raw_1, 3)
+
+        supply_stacks.operate_crane_mover_9000(actual_1,
+                                               'move 1 from 2 to 1')
+
+        expected_1 = {1: ['D', 'N', 'Z'],
+                      2: ['C', 'M'],
+                      3: ['P']}
+
+        self.assertDictEqual(actual_1, expected_1)
+
+        # Test all remaining instructions on example
+        supply_stacks.operate_crane_mover_9000(actual_1,
+                                               'move 3 from 1 to 3')
+        supply_stacks.operate_crane_mover_9000(actual_1,
+                                               'move 2 from 2 to 1')
+        supply_stacks.operate_crane_mover_9000(actual_1,
+                                               'move 1 from 1 to 2')
+
+        expected_2 = {1: ['C'],
+                      2: ['M'],
+                      3: ['Z', 'N', 'D', 'P']}
+
+        self.assertDictEqual(actual_1, expected_2)
+
+    def test_operate_crate_mover_9001(self):
+
+        # Test first instruction on example stack
+        raw_1 = ['    [D]    ',
+                 '[N] [C]    ',
+                 '[Z] [M] [P]']
+
+        actual_1 = supply_stacks.parse_stacks(raw_1, 3)
+
+        supply_stacks.operate_crane_mover_9001(actual_1,
+                                               'move 1 from 2 to 1')
+
+        expected_1 = {1: ['D', 'N', 'Z'],
+                      2: ['C', 'M'],
+                      3: ['P']}
+
+        self.assertDictEqual(actual_1, expected_1)
+
+        # Test all remaining instructions on example
+        supply_stacks.operate_crane_mover_9001(actual_1,
+                                               'move 3 from 1 to 3')
+        supply_stacks.operate_crane_mover_9001(actual_1,
+                                               'move 2 from 2 to 1')
+        supply_stacks.operate_crane_mover_9001(actual_1,
+                                               'move 1 from 1 to 2')
+
+        expected_2 = {1: ['M'],
+                      2: ['C'],
+                      3: ['D', 'N', 'Z', 'P']}
+
+        self.assertDictEqual(actual_1, expected_2)
 
 
 if __name__ == '__main__':
